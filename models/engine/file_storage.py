4e01507c0fc682f495fe6296ed0843085e130980
Eidal559 <eidaljuarbe@gmail.com>
2024-04-19 10:43:33 -0400

    updated file

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -69,7 +69,7 @@ class FileStorage:
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
 
-   def get(self, cls, id):
+    def get(self, cls, id):
         """A method to retrieve one object"""
         obj = self.all(cls)
         for key in obj.keys():
