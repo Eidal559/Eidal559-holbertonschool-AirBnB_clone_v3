b23c6fbaea4a2f63d0ef91884d5ff0c52633c448
Elizabeth <elizabeth.zabala3@gmail.com>
2024-04-19 07:14:52 -0400

    changing everything back

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python3
 """Import blueprint from flask"""
 from flask import Blueprint
 
@@ -11,4 +10,3 @@ from api.v1.views.amenities import *
 from api.v1.views.users import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
-from api.v1.views.places_amenities import *
